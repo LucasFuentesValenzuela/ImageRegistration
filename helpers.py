d5b1926758de184662f65496e3290761c1bae153
LucasFuentesValenzuela <lucas.fuentes-valenzuela@outlook.com>
2019-10-18 12:20:09 -0700

    Managing the 2-part image, with temporary fixes

--- helpers.py
+++ helpers.py
@@ -8,12 +8,18 @@ import tifffile as tiff
 
 from astroalign_test import *
 
-def flip_rotate_images(Folders):
-    for filename in os.listdir(Folders['HighMag']):
+def flip_rotate_images(Folders,partnb):
+
+    assert partnb in [1,2]
+    key='HighMag_part'+str(partnb)
+    print("Performing RF in following directory:", Folders[key])
+
+    for filename in os.listdir(Folders[key]):
+        
         if filename.endswith(".tif"): 
             print(filename)
             #read only the first channel, which corresponds to DAPI
-            img=tiff.imread(Folders['HighMag']+filename)
+            img=tiff.imread(Folders[key]+filename)
             img=img[1,:,:]
             img=img.transpose()
             img=cv2.flip(img,0)
@@ -22,35 +28,49 @@ def flip_rotate_images(Folders):
             nb1=int(nbrs[0:3])
             nb2=int(nbrs[4:8])
             new_name=str(nb1)+'_'+str(nb2)+'.tif'
-            cv2.imwrite(Folders['HighMag_RF']+new_name,img)
+            cv2.imwrite(Folders['HighMag_RF']+'part_'+str(partnb)+'/'+new_name,img)
         else:
             continue
 
 
 
-def concatenate_crop_ROI(nb_pixel_x, nb_pixel_y,delta,fov_oct,Folders,offset):
+def concatenate_crop_ROI(nb_pixel,delta,fov_oct,Folders,offset):
     #concatenate FOVs, then crop ROI
 
     pixel_size=0.365623*10**-6
     size_cropping=fov_oct+2*delta
     n_pixels_map=round(size_cropping/pixel_size)
-    fov_nh_x=nb_pixel_x*pixel_size#nh = nighthawk, i.e. high mag
-    fov_nh_y=nb_pixel_y*pixel_size
+    fov_nh_x=nb_pixel['x']*pixel_size#nh = nighthawk, i.e. high mag
+    fov_nh_y=nb_pixel['y']*pixel_size
 
     for filename in os.listdir(Folders['lowMag']):
         if 'fluorescent' in filename:
             
             print(filename)
-            low_mag_id_1=int(filename[1:3])
-            low_mag_id_2=int(filename[4:6])
-            y_start=max(low_mag_id_1*fov_oct-delta,0)+offset['y']
-            x_start=max(low_mag_id_2*fov_oct-delta,0)+offset['x']
+            low_mag_id_1=int(filename[1:3])#along y
+            low_mag_id_2=int(filename[4:6])#along x
+
+            #here, we apply the offset depending on which part of the scan we are using
+            #it is a temporary fix until we can to handle the overlap properly
+            #it needs a bit more thinking
+            if low_mag_id_2 < 5:
+                y_start=max(low_mag_id_1*fov_oct-delta,0)+offset['y']
+                x_start=max(low_mag_id_2*fov_oct-delta,0)+offset['x']
+                partnb=1
+            elif low_mag_id_2 >6 : 
+                y_start=max(low_mag_id_1*fov_oct-delta,0)+offset['y']#-offset['y_part2']
+                x_start=max(low_mag_id_2*fov_oct-delta,0)+offset['x']#-offset['x_part2']
+                partnb=2
+            else:
+                continue #currently ignoring some files
+
+
             y_end=y_start+fov_oct+2*delta
             x_end=x_start+fov_oct+2*delta
             
             #loop through the frames
             #identify which ones are the limit ones
-            nb_im_x=18
+            nb_im_x=21
 
             for i in range(nb_im_x):
                 if i*fov_nh_x<=x_start and (i+1)*fov_nh_x>=x_start:
@@ -62,38 +82,67 @@ def concatenate_crop_ROI(nb_pixel_x, nb_pixel_y,delta,fov_oct,Folders,offset):
                 if i*fov_nh_y<=y_end and (i+1)*fov_nh_y>=y_end:
                     frame_y_end=i+1
             
+            # print("x_frames: ", frame_x_start, frame_x_end)
+            #compatibility check
+            if frame_x_start<=10 and frame_x_end>10: #it needs frames from both parths
+                print("This frame has not been neglected, it needs FOVs from both parts!")
+                continue
+            elif frame_x_start>10 and partnb==2:  #we are just changing the name for proper loading, surely to be changed afterwards when dealing with overlap
+                print("x_frames: ", frame_x_start, frame_x_end)
+                frame_x_start-=10
+                frame_x_end-=10
+                print("x_frames: ", frame_x_start, frame_x_end)
+                #assuming there is an overlap of one row between part 1 and 2
+                frame_x_start+=1
+                frame_x_end+=1
+                print("x_frames: ", frame_x_start, frame_x_end)
+
+            
+
+            
+
+            
             #create blank picture
-    #         full_image=np.zeros((n_pixels_map,n_pixels_map))
-            full_image=np.zeros((nb_pixel_y*(frame_y_end-frame_y_start+1),nb_pixel_x*(frame_x_end-frame_x_start+1)))
-            #pad picture with corresponding frames
+            full_image=np.zeros((nb_pixel['y']*(frame_y_end-frame_y_start+1),nb_pixel['x']*(frame_x_end-frame_x_start+1)))
+            print("theory shape: ", full_image.shape)
             idx_x=np.arange(frame_x_end,frame_x_start-1,-1)
             idx_y=np.arange(frame_y_end,frame_y_start-1,-1)
 
             for i in range(idx_x.shape[0]):
                 for j in range(idx_y.shape[0]):
                     filename=str(idx_y[j])+'_'+str(idx_x[i])+'.tif'
-                    if os.path.exists(Folders['HighMag_RF']+filename):
-                        crt_img=cv2.imread(Folders['HighMag_RF']+filename,0)
+                    if os.path.exists(Folders['HighMag_RF']+'part_'+str(partnb)+'/'+filename):
+                        crt_img=cv2.imread(Folders['HighMag_RF']+'part_'+str(partnb)+'/'+filename,0)
                     else:
+                        print("file does not exist")
                         continue
-                    full_image[j*nb_pixel_y:(j+1)*nb_pixel_y,i*nb_pixel_x:(i+1)*nb_pixel_x]=crt_img
-                    
+                    full_image[j*nb_pixel['y']:(j+1)*nb_pixel['y'],i*nb_pixel['x']:(i+1)*nb_pixel['x']]=crt_img
+
+                    #determine the shift for the crop region 
                     if i==0 and j==0:
-                        x_start_crt=nb_pixel_x*(idx_x[i]-1)*pixel_size
-                        y_start_crt=nb_pixel_y*(idx_y[j]-1)*pixel_size
-                        x_ref=x_start_crt+nb_pixel_x*pixel_size
-                        y_ref=y_start_crt+nb_pixel_y*pixel_size
+
+                        #this condition is to properly determine what area of the region you want to crop
+                        #super important to have it correctly coded, as otherwise you risk extracting the wrong frames
+                        #in this case we add 10-1 because we did the same thing with the frames above
+                        #to be cleaned and improved
+                        if partnb==2: 
+                            x_start_crt=nb_pixel['x']*(idx_x[i]-1+10-1)*pixel_size
+                        else: 
+                            x_start_crt=nb_pixel['x']*(idx_x[i]-1)*pixel_size
+                        y_start_crt=nb_pixel['y']*(idx_y[j]-1)*pixel_size
+                        x_ref=x_start_crt+nb_pixel['x']*pixel_size
+                        y_ref=y_start_crt+nb_pixel['y']*pixel_size
                         x_idx=int(round((x_ref-x_end)/pixel_size))
                         y_idx=int(round((y_ref-y_end)/pixel_size))
             
             full_image=full_image[y_idx:y_idx+n_pixels_map,x_idx:x_idx+n_pixels_map]
-                    
+            # print(y_idx+n_pixels_map)
+            # print(x_idx+n_pixels_map)
             
             full_image_name=str(low_mag_id_1)+'_'+str(low_mag_id_2)+str('.png')
             cv2.imwrite(Folders['Virtual']+full_image_name,full_image)
             print(full_image_name)
             print(full_image.shape)
-            print("---------------")
         else:
             continue
 
